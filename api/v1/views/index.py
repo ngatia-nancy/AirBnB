f766d45f704f7bc6fa93d727ffa6e819b6dc5172
ngatia-nancy <111184362+ngatia-nancy@users.noreply.github.com>
2023-05-29 21:33:34 +0300

    Designing a RESTful api with python and flask

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -24,9 +24,12 @@ def status():
 @app_views.route('/stats', strict_slashes=False)
 def stats():
     """Endpoint that retrieves the number of each objects by type"""
-    return jsonify({"amenities": storage.count("Amenity"),
-                    "cities": storage.count("City"),
-                    "places": storage.count("Place"),
-                    "reviews": storage.count("Review"),
-                    "states": storage.count("State"),
-                    "users": storage.count("User")})
+    return jsonify(
+        {
+            "amenities": storage.count("Amenity"),
+            "cities": storage.count("City"),
+            "places": storage.count("Place"),
+            "reviews": storage.count("Review"),
+            "states": storage.count("State"),
+            "users": storage.count("User")
+        })
