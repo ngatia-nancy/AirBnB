5142e820304190802854be9cae9083e6cdb3e0bb
ngatia-nancy <111184362+ngatia-nancy@users.noreply.github.com>
2023-05-29 20:52:33 +0300

    Designing a RESTful api with python and flask

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -18,7 +18,7 @@ from models.user import User
 @app_views.route('/status', strict_slashes=False)
 def status():
     """Returns the status of the API"""
-    return jsonify({"status": "OK"})
+    return jsonify({'status': 'OK'})
 
 
 @app_views.route('/stats', strict_slashes=False)
