42efce781ec25bc66949859b1b92b46389474bf1
ngatia-nancy <111184362+ngatia-nancy@users.noreply.github.com>
2023-05-30 10:51:41 +0300

    Designing a RESTful api with python and flask

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 """
-    A route /status on the object app_views that returns a
-    status: OK JSON
+Flask Web app
 """
-from flask import jsonify
 from api.v1.views import app_views
+from flask import jsonify
 from models import storage
 from models.amenity import Amenity
 from models.city import City
@@ -15,29 +14,20 @@ from models.user import User
 
 
 @app_views.route('/status', strict_slashes=False)
-def app_status():
-    """
-         a route /status on the object app_views that returns
-         a JSON: "status": "OK"
-    """
+def status():
+    """Returns the status of the API"""
     return jsonify({'status': 'OK'})
 
 
 @app_views.route('/stats', strict_slashes=False)
-def all_stats():
-    """
-        Create an endpoint that retrieves the number of each objects by type
-    """
-    obj = {
-            "amenities": storage.count(Amenity),
-            "cities": storage.count(City),
-            "places": storage.count(Place),
-            "reviews": storage.count(Review),
-            "states": storage.count(State),
-            "users": storage.count(User),
-        }
-    return jsonify(obj)
-
-
-if __name__ == "__main__":
-    pass
+def stats():
+    """Endpoint that retrieves the number of each objects by type"""
+    return jsonify(
+        {
+            "amenities": storage.count("Amenity"),
+            "cities": storage.count("City"),
+            "places": storage.count("Place"),
+            "reviews": storage.count("Review"),
+            "states": storage.count("State"),
+            "users": storage.count("User")
+        })
